a44fa5e5529c5de2b31c2d75e7ae9d6559c3101e
git1239 <108423449+git1239@users.noreply.github.com>
2022-08-10 21:30:52 -0400

    Update 14.LongestCommonPrefix.py

--- 14.LongestCommonPrefix.py
+++ 14.LongestCommonPrefix.py
@@ -1,1 +1,15 @@
 
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        shortest = min(strs,key=len)
+        for i, ch in enumerate(shortest):
+            for other in strs:
+                if other[i] != ch:
+                    return shortest[:i]
+        return shortest 
