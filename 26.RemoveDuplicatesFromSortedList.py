1ccb4d440506fd7725d83fdd7521b8ca823f7baa
git1239 <108423449+git1239@users.noreply.github.com>
2022-08-19 11:47:29 +0100

    Update 26.RemoveDuplicatesFromSortedList.py

--- 26.RemoveDuplicatesFromSortedList.py
+++ 26.RemoveDuplicatesFromSortedList.py
@@ -1,5 +1,5 @@
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
+    def removeDuplicates(self, nums):
         if len(nums) == 0: return 0
         if len(nums) == 1: return 1
         
