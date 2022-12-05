4d5cbcb716fbf3f3f2f5e2e0016c79f9636cfbec
git1239 <108423449+git1239@users.noreply.github.com>
2022-12-05 21:03:55 +0000

    Create 145.BinaryTreePostorderTraversal.py

---
+++ 145.BinaryTreePostorderTraversal.py
@@ -0,0 +1,9 @@
+def postorderTraversal(self, root):
+    res, stack = [], [root]
+    while stack:
+        node = stack.pop()
+        if node:
+            res.append(node.val)
+            stack.append(node.left)
+            stack.append(node.right)
+    return res[::-1]
